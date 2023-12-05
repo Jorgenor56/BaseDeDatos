4a37cc6f2348d1b1ce9e6f09fb40a5104d894fad
Jorgenor56 <132491623+Jorgenor56@users.noreply.github.com>
2023-12-05 12:26:14 +0100

    BaseDeDatosAD.java

--- BaseDeDatosAD/src/main/java/com/mycompany/basededatosad/BaseDeDatosAD.java
+++ BaseDeDatosAD/src/main/java/com/mycompany/basededatosad/BaseDeDatosAD.java
@@ -4,22 +4,27 @@ import java.sql.*;
 
 public class BaseDeDatosAD {
 
+    //Declaro constantes para la URL de la base de datos
     static final String DB_URL = "jdbc:mysql://localhost:3306/jcvd";
     static final String USER = "dam2";
     static final String PASS = "1234";
     static final String QUERY = "SELECT * FROM videojuegos";
 
     public static void main(String[] args) {
-        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        //Establezco la conexion con la base de datos
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS); 
+             //Creo un objeto Statement para enviar consultas a la base de datos
              Statement stmt = conn.createStatement()) {
 
+            //Creo una consulta SQL para insertar un nuevo registro en la tabla videojuegos
             String insertQuery = "INSERT INTO videojuegos "
             + "(Nombre, Genero, FechaLanzamiento, Compañia, Precio) VALUES "
             + "('Pokemon', 'Plataforma', '2018-06-01', 'Nintendo', 19.99)";
-            stmt.executeUpdate(insertQuery);
-
-            ResultSet rs = stmt.executeQuery(QUERY);
+            stmt.executeUpdate(insertQuery); //Ejecuto la consulta de inserción
+            //Ejecuto la consulta QUERY y guarda el resultado en rs
+            ResultSet rs = stmt.executeQuery(QUERY); 
             while (rs.next()) {
+                //Imprimo los datos de cada columna de la fila actual
                 System.out.println("-ID:" + rs.getInt("id"));
                 System.out.println(", Nombre:" + rs.getString("Nombre"));
                 System.out.println(", Genero:" + rs.getString("Genero"));
@@ -27,9 +32,9 @@ public class BaseDeDatosAD {
                 System.out.println(", Compañia:" + rs.getString("Compañia"));
                 System.out.println(", Precio:" + rs.getFloat("Precio"));
             }
-          stmt.close();
+          stmt.close(); //Cierro el objeto Statement
         } catch (SQLException e) {
-            e.printStackTrace();
+            e.printStackTrace(); //Imprimo la excepción en caso de error
         }
     }
 }
